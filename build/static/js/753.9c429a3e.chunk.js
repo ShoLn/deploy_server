14bcaabf2fcf5332cef09a7b3ef9dd71e6324e21
Shawn Lin <T22035@Oring.com.tw>
2022-09-13 15:25:54 +0800

    build

---
+++ build/static/js/753.9c429a3e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprogressive_exercise=self.webpackChunkprogressive_exercise||[]).push([[753],{1753:function(e,r,s){s.r(r);s(2791);var n=s(184);r.default=function(){return(0,n.jsx)("div",{children:"InventoryPage"})}}}]);
+//# sourceMappingURL=753.9c429a3e.chunk.js.map
---
+++ build/static/js/788.6a0af0fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprogressive_exercise=self.webpackChunkprogressive_exercise||[]).push([[788],{9788:function(e,r,s){s.r(r);s(2791);var i=s(184);r.default=function(){return(0,i.jsx)("div",{children:"FieldPage"})}}}]);
+//# sourceMappingURL=788.6a0af0fc.chunk.js.map
